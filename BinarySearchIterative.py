f0b7c349b40a65b1a71839dd308f13b4204993e5
Orkun Sürücüoglu <orkun.surucuoglu@gmail.com>
2024-05-21 07:46:10 +0200

    Iterative Binary Search Implementation and test code

---
+++ BinarySearchIterative.py
@@ -0,0 +1,38 @@
+from random import randint
+
+def binary_search(arr: list[int], x: int) -> int:
+    if arr is None:
+        return -1
+        
+    l, r = 0, len(arr) - 1
+    
+    while l <= r:
+        mid = l + (r - l) // 2
+        if x == arr[mid]:
+            return mid
+        elif x < arr[mid]:
+            r = mid - 1
+        else:
+            l = mid + 1
+    
+    return -1
+    
+if __name__ == '__main__':
+    arr = []
+    for i in range(30):
+        arr.append(randint(1,100))
+    
+    arr.sort()
+    #print('arr: ' + str(arr))
+    x = randint(1,100)
+    #print('x: ' + str(x))
+    #print('')
+    
+    index = binary_search(arr, x)
+    if index == -1:
+        print(f'{x} does not exist in {arr}')
+    else:
+        print(f'{x} exists in {arr}')
+        
+        
+    
